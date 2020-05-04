ac46042a12d6d792280d4b06fba13c0151e2d417
Dilshan-Madhushanka <46591533+teddiya@users.noreply.github.com>
2020-05-04 17:32:29 +0530

    Add adminHome jasp page and create login validation using phpmyAdmin database.

---
+++ PAF-Assignment2020/src/com/Logincheck.java
@@ -0,0 +1,86 @@
+package com;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Logincheck
+ */
+@WebServlet("/Logincheck")
+public class Logincheck extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Logincheck() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+//		String uid = request.getParameter("id");
+//		String password = request.getParameter("pass");
+//		System.out.println(request);
+//		if (uid.equals("123") && password.equals("123")) {
+//			response.sendRedirect("adminHome.html");
+//			return;
+//		} else {
+//			response.sendRedirect("error.jsp");
+//			return;
+//		}
+		String un=request.getParameter("id");
+		String pw=request.getParameter("pass");
+		
+		// Connect to mysql and verify admincode password
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		 // loads driver
+		Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf-assignment", "root", ""); // gets a new connection
+ 
+		PreparedStatement ps = c.prepareStatement("select adminCode,adminPass from admin where adminCode=? and adminPass=?");
+		ps.setString(1, un);
+		ps.setString(2, pw);
+ 
+		ResultSet rs = ps.executeQuery();
+ 
+		while (rs.next()) {
+			response.sendRedirect("adminHome.html");
+			return;
+		}
+		response.sendRedirect("error.jsp");
+		return;
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
